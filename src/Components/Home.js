66c0466f0a087f147edf874e692e1c5dbcd64cf8
a-blaine <ai.blaine4@protonmail.com>
2023-06-26 16:48:23 +0100

    Created quiz and home components

---
+++ src/Components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <button>Start Quiz!</button>
+    </div>
+  );
+}
---
+++ src/Components/Quiz.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Quiz() {
+  return (
+    <div className="quiz">
+      <h2>Q1. What is the word for horse in Spanish?</h2>
+      <ul>
+        <li>Cabra</li>
+        <li>Caballo</li>
+        <li>Perro</li>
+        <li>Gatito</li>
+      </ul>
+    </div>
+  );
+}
