430e17f6060c204e5e80949e4a7c2a74efeee7c0
a-blaine <ai.blaine4@protonmail.com>
2023-06-26 17:08:00 +0100

    Created endscreen component and Context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import "./App.css";
+import EndScreen from "./Components/EndScreen";
+import Home from "./Components/Home";
+import Quiz from "./Components/Quiz";
+import { QuizContext } from "./Helpers/Contexts";
 
 function App() {
+  const [quizState, setQuizState] = useState("home");
+
   return (
     <div className="App">
       <h1>Quiz App</h1>
+      <QuizContext.Provider value={{ quizState, setQuizState }}>
+        {quizState === "home" && <Home />}
+        {quizState === "quiz" && <Quiz />}
+        {quizState === "endScreen" && <EndScreen />}
+      </QuizContext.Provider>
     </div>
   );
 }
---
+++ src/Components/EndScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EndScreen() {
+  return (
+    <div className="EndScreen">
+      <h2>You're finished!</h2>
+    </div>
+  );
+}
